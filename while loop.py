c772a29aff80e771ec2b3a8be43b55423cc6ace9
ijalapoojitha <ijalapoojitha@gmail.com>
2025-12-26 15:49:24 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,16 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+r=0
+while r<4:
+    c=0
+    while c<5:
+        print ("r=",r,"c=",c)
+        c+=1
+        r+=1
+print(c)
+print(r)
