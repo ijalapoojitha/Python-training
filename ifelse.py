25cd988dffff52c929031e707146444b13595e12
ijalapoojitha <ijalapoojitha@gmail.com>
2025-12-27 09:22:04 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+x=int(input())
+if x%5==0:
+    print("yes")
+else:
+    print("no")
